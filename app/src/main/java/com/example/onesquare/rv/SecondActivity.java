3b220e69537b7239a4bfee29a8abfcb313f0a9ae
SwapnaPrakash <swapna1569@gmail.com>
2017-12-01 16:17:51 +0530

    Second Activity Added

---
+++ app/src/main/java/com/example/onesquare/rv/SecondActivity.java
@@ -0,0 +1,18 @@
+package com.example.onesquare.rv;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ONESQUARE-2 on 12/1/2017.
+ */
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+    }
+}
